d254ae204d7044e53659dce4358c084a925fcfe6
dodumu <davidrobertq2@gmail.com>
2026-09-06 22:30:31 +0100

    middleware integration

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"jobFlow/database"
+	"jobFlow/middleware"
 	"log"
+	"net/http"
 )
 
+func DashboardHandler(w http.ResponseWriter, r *http.Request) {
+	userID := r.Context().Value(middleware.UserIDKey)
+	fmt.Fprintf(w, "Welcome %v", userID)
+}
 func main() {
 	err := database.InitDB("database.db")
 	if err != nil {
 		log.Println(err)
 	}
+
+	http.Handle("/dashboard", middleware.AuthMiddleware(http.HandlerFunc(DashboardHandler)))
+	log.Println("server is running on :8081")
+	http.ListenAndServe(":8081", nil)
 }
